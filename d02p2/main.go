93820d6546e12f63837214872168fa74f16b2a70
Sean Nichols <git@seanich.com>
2022-12-02 21:27:14 -0500

    day 2 part 2

--- d02p2/main.go
+++ d02p2/main.go
@@ -15,11 +15,8 @@ const (
 
 var moveMap = map[rune]int{
 	'A': MoveRock,
-	'X': MoveRock,
 	'B': MovePaper,
-	'Y': MovePaper,
 	'C': MoveScissors,
-	'Z': MoveScissors,
 }
 
 const (
@@ -27,6 +24,12 @@ const (
 	OutcomeDraw
 	OutcomeWin
 )
+
+var outcomeMap = map[rune]int{
+	'X': OutcomeLoss,
+	'Y': OutcomeDraw,
+	'Z': OutcomeWin,
+}
 
 func main() {
 	f, err := os.Open("input.txt")
@@ -39,15 +42,15 @@ func main() {
 	var score int
 	for s.Scan() {
 		runes := []rune(s.Text())
-		theirMove, myMove := moveMap[runes[0]], moveMap[runes[2]]
-		score += myMove
-
-		if theirMove == myMove {
-			score += OutcomeDraw
-		} else if myMove == (theirMove)%3+1 {
-			score += OutcomeWin
-		} else {
-			score += OutcomeLoss
+		theirMove, myOutcome := moveMap[runes[0]], outcomeMap[runes[2]]
+		score += myOutcome
+		switch myOutcome {
+		case OutcomeDraw:
+			score += theirMove
+		case OutcomeLoss:
+			score += (theirMove+1)%3 + 1
+		case OutcomeWin:
+			score += theirMove%3 + 1
 		}
 	}
 
